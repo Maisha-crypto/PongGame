3d91924a1bd9043a46f5db67b161ce93bbc995d0
Maisha-crypto <maishasambopape@gmail.com>
2023-07-29 10:07:34 +0200

    create and update the score whenever each paddle collides or misses the ball

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,25 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
-class scoreboard(Turtle):
+class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.score_update()
+
+
+    def score_update(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", "80", "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", "80", "normal"))
+
+    def left_point(self):  
+        self.l_score += 1
+
+    def right_point(self):
+        self.r_score += 1
