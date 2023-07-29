f5ee5e3995a247e31069b02083e7abe805a055ae
Maisha-crypto <maishasambopape@gmail.com>
2023-07-29 10:05:03 +0200

    create the ball speed attribute and update it everytime the ball collides with either paddle

--- ball.py
+++ ball.py
@@ -9,6 +9,7 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.ball_speed = 0.01
 
     def move_ball(self):
         new_x = self.xcor() + self.x_move
@@ -17,11 +18,17 @@ class Ball(Turtle):
 
     def wall_bounce(self):
         self.y_move *= -1
+        self.ball_speed *= 0.9
 
     def paddle_bounce(self):
+        self.ball_speed *= 0.9
         self.x_move *= -1
 
     def reset_ball(self):
         self.goto(0,0)
+        self.ball_speed = 0.1
         self.paddle_bounce()
+
+        
+
         
