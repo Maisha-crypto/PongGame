481f88a52378f9cc70c46e86acaf75b8a1452090
Maisha-crypto <maishasambopape@gmail.com>
2023-07-29 10:01:52 +0200

    create an instatnce of the scoreboad class, Update sleep time,

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from scoreboard import Scoreboard
 from paddle import Paddle
 from time import sleep
 from ball import Ball
@@ -17,6 +18,9 @@ right_paddle = Paddle((380, 0))
 # Creating an intance of the ball class
 ball = Ball()
 
+# Creating an instance of the scoreboard class
+scoreboard = Scoreboard()
+
 # Activating the screen listen() function.
 my_screen.listen()
 
@@ -29,37 +33,38 @@ my_screen.onkey(left_paddle.goUp, 'w')
 my_screen.onkey(left_paddle.goDown, 's')
 
 isGameOn = True
+SLEEP_TIME = 0.1
 
 while isGameOn:
     my_screen.update()
-    sleep(0.1)
+    sleep(SLEEP_TIME)
     ball.move_ball()
 
     # checking if the ball colides with the wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.wall_bounce()
 
-    # checking if the ball colides with the paddle
+    # checking if the ball colides with the right paddle
     if ball.distance(right_paddle)< 50 and ball.xcor() > 350:
         ball.paddle_bounce()
-
+        scoreboard.right_point()
+        scoreboard.score_update()
+           
+    # checking if the ball collides with the left paddle
     if ball.distance(left_paddle) < 50 and ball.xcor() < -360:
         ball.paddle_bounce()
+        scoreboard.left_point()
+        scoreboard.score_update()     
+             
     # checking if the paddle misses the ball
     if ball.xcor() > 400:
         ball.reset_ball()
-
+        scoreboard.left_point()
+        scoreboard.score_update()
+        
     if ball.xcor() < -400:
         ball.reset_ball()
-        
-
-
-   
-
-
-
-
-
-
-
+        scoreboard.right_point()
+        scoreboard.score_update()
+          
 my_screen.exitonclick()
